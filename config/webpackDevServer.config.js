d5021c265ede67274ba5bdaa3b3c1e2a3f0074e6
lzw806414685 <806414685@qq.com>
2020-09-03 17:09:06 +0800

    1234

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -101,7 +101,15 @@ module.exports = function(proxy, allowedHost) {
     },
     public: allowedHost,
     // `proxy` is run between `before` and `after` `webpack-dev-server` hooks
-    proxy,
+    proxy:{
+      "/api":{
+        target:"http://47.96.0.211:9000",
+        changeOrigin:true,
+        pathRewrite:{
+          "^/api":""
+        }
+      }
+    },
     before(app, server) {
       // Keep `evalSourceMapMiddleware` and `errorOverlayMiddleware`
       // middlewares before `redirectServedPath` otherwise will not have any effect
